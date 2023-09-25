f8d5fcee770f2dce090a28fca59b8a42cbe66b2b
karthixv4 <100293740+karthixv4@users.noreply.github.com>
2023-09-25 21:58:24 +0530

    implementing a linkedlist - 1

---
+++ src/main/java/linkedList/MainMethod.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+public class MainMethod {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(50);
+        list.insert(51);
+        list.insert(52);
+        list.print();
+    }
+}
