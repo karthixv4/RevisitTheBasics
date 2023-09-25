3ac8177b3b80c3b28dfd58afe4dc33fa561e38bd
karthixv4 <100293740+karthixv4@users.noreply.github.com>
2023-09-25 22:21:24 +0530

    implementing a linkedlist - 2

--- src/main/java/linkedList/LinkedList.java
+++ src/main/java/linkedList/LinkedList.java
@@ -26,4 +26,29 @@ public class LinkedList {
         }
         System.out.println(node.data);
     }
+
+    public void insertAtStart(int data){
+        Node node = new Node();
+        node.data = data;
+        node.next = head;
+        head = node;
+    }
+
+    public void insertAtSpecific(int index, int data){
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+        Node n = head;
+        if(index == 0){
+            node.next = n;
+            head = node;
+            //or insertAtStart(data)
+        }
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+            node.next = n.next;
+            n.next = node;
+    }
+
 }
--- src/main/java/linkedList/MainMethod.java
+++ src/main/java/linkedList/MainMethod.java
@@ -6,6 +6,8 @@ public class MainMethod {
         list.insert(50);
         list.insert(51);
         list.insert(52);
+        list.insertAtStart(100);
+        list.insertAtSpecific(0,15);
         list.print();
     }
 }
